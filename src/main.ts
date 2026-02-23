2f3f7da96445dc37934100ed5f8e50f63dce84a3
jmg24a <jmg24a@gmail.com>
2026-02-22 22:37:05 -0400

    add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ 
+    origin: ['https://muebleriatiojaime.vercel.app'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: 'GET,POST,PUT,PATCH,DELETE', 
+    credentials: false, 
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
