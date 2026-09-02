88c9575e98f03514eb4cafa56a9c05916415ab79
jmg24a <jmg24a@gmail.com>
2026-09-02 17:33:24 -0400

    sa

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,11 +1,11 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Product } from "./product.entity";
+import { Products } from "./product.entity";
 import { ProductsService } from "./products.service";
 import { ProductsController } from "./products.controller";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Products])],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
